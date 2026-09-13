ba8142a9f41b44762b4540d6dd16f5140067a8b0
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-09-13 18:18:24 -0500

    Complete testimonial slider user interface

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,17 @@
 
 document.addEventListener('DOMContentLoaded', () => {
    console.log('DOMContentLoaded is loaded and ready to use');
+
+   const testimonialSlider = document.getElementById('testimonial-slider');
+   const testimonialCarousel = document.getElementById('testimonial-carousel');
+   const prevButton = document.getElementById('prev-button');
+   const nextButton = document.getElementById('next-button');
+
+   const firstCard = testimonialCarousel.querySelector('.testimonial-card')
+   const firstCardWidth = firstCard.offsetWidth;
+
+   let isDragging = false;
+   let startX;
+   let startScrollLeft;
+
 });
