4f0537bb7a802c475bc2173191cf2b73a56f9a8b
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-09-15 07:41:05 -0500

    Complete hotfix/html-css with addition of preloader

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,36 @@
 'use strict';
 
+/*===============================================================
+          preloader scripts
+ ==================================================================*/
+document.addEventListener('DOMContentLoaded', function () {
+   // makes sure that whole site is loaded, so the 3000ms preloader coincides with
+   // the delay of 3000ms for the header content animation
+   console.log('DOMContentLoaded for the preloader');
+
+   const preloader = document.getElementById('preloader');
+   const preloader_gif = document.getElementById('preloader-gif');
+
+
+   if (preloader_gif) {
+      preloader_gif.style.transition = 'opacity 3000ms ease-in-out';
+      preloader_gif.style.opacity = '0';
+
+   }
+   if (preloader) {
+      preloader.style.transition = 'opacity 3000ms ease-in-out';
+      preloader.style.opacity = '0';
+   }
+
+   setTimeout(function () {
+      preloader_gif.style.display = 'none';
+      preloader.style.display = 'none';
+   }, 3000);
+});
+
+/*===============================================================
+          main scripts
+ ==================================================================*/
 document.addEventListener('DOMContentLoaded', () => {
    console.log('DOMContentLoaded and ready for use!');
 
